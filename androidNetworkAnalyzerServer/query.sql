db136598b9d87b3d8627ba983b7b0c4f1d846cd9
Clara Bou Mansour <ceb11@mail.aub.edu>
2022-12-03 14:33:19 +0200

    added sql-connector and sql code for creation of table

---
+++ androidNetworkAnalyzerServer/query.sql
@@ -0,0 +1,12 @@
+create table cell_information (
+	network_operator varchar(50),
+    signal_power real,
+    SINR real,
+    SNR real,
+    network_type varchar(5),
+    frequency_band real,
+    cell_id varchar(50),
+    time_stamp datetime,
+    mobile_id varchar(50)
+
+);
